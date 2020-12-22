2624251e1ef85b28f7fa4ccbf7f4864882c0d1d9
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2020-12-22 20:06:53 +0000

    "added a feature extraction function to Gradient Ascent"

--- GNN_framework/overall_algorithm.py
+++ GNN_framework/overall_algorithm.py
@@ -6,8 +6,8 @@ import torch
 # This function acts aims to find adversarial examples for each property in the file specified. It acts as a container
 # for the function which attacks each property in turn by calling this function for each property.
 
-def pgd_attack_properties(properties_filename, model_name, epsilon_factor, pgd_learning_rate, num_epochs, num_trials,
-                          subset=None):
+def pgd_gnn_attack_properties(properties_filename, model_name, epsilon_factor, pgd_learning_rate, num_epochs,
+                              num_trials, subset=None):
     # Load all the required data for the images which were correctly verified by the model
     images, true_labels, image_indices, model = load_cifar_data(model_name)
 
@@ -18,20 +18,15 @@ def pgd_attack_properties(properties_filename, model_name, epsilon_factor, pgd_l
 
     # If the subset of indices was specified for the purpose of reducing the time complexity, drop the elements of
     # images, true_labels, test_labels and epsilons not indicated in the subset indices
-    original_length = len(images)
     if subset is not None:
-        for i in range(original_length - 1, -1, -1):
-            if i not in subset:
-                images.pop(i)
-                true_labels.pop(i)
-                test_labels.pop(i)
-                epsilons.pop(i)
+        images, true_labels, test_labels, epsilons = match_with_subset(subset, images, true_labels, test_labels,
+                                                                       epsilons)
 
     # Now attack each property in turn by calling the appropriate function
     num_properties_still_verified = 0  # counter of properties which are still verified after the PGD attack
     for i in range(len(images)):
-        successful_attack_flag = pgd_attack_property(model, images[i], true_labels[i], test_labels[i], epsilons[i],
-                                                     epsilon_factor, pgd_learning_rate, num_epochs, num_trials)
+        successful_attack_flag = pgd_gnn_attack_property(model, images[i], true_labels[i], test_labels[i], epsilons[i],
+                                                         epsilon_factor, pgd_learning_rate, num_epochs, num_trials)
 
         # If the attack was unsuccessful, increase the counter
         if not successful_attack_flag:
@@ -43,12 +38,24 @@ def pgd_attack_properties(properties_filename, model_name, epsilon_factor, pgd_l
     return verification_accuracy
 
 
+# This function updates the lists of images, true labels, test labels and epsilons based on the given subset of indices
+
+def match_with_subset(subset, images, true_labels, test_labels, epsilons):
+    original_length = len(images)
+    for i in range(original_length - 1, -1, -1):
+        if i not in subset:
+            images.pop(i)
+            true_labels.pop(i)
+            test_labels.pop(i)
+            epsilons.pop(i)
+    return images, true_labels, test_labels, epsilons
+
+
 # This function performs the PGD attack on the specified property characterised by its image, true label, test label and
 # epsilon value
 
-def pgd_attack_property(model, image, true_label, test_label, epsilon, epsilon_factor, pgd_learning_rate, num_epochs,
-                        num_trials):
-
+def pgd_gnn_attack_property(model, image, true_label, test_label, epsilon, epsilon_factor, pgd_learning_rate,
+                            num_epochs, num_trials):
     # First, perturb the image randomly within the allowed bounds and perform a PGD attack
     lower_bound = torch.add(-epsilon * epsilon_factor, image)
     upper_bound = torch.add(epsilon * epsilon_factor, image)
@@ -62,16 +69,17 @@ def pgd_attack_property(model, image, true_label, test_label, epsilon, epsilon_f
     if successful_attack_flag:
         return True
 
-    return
+    return  # TODO
 
 
-# This function performs Gradient Ascent on the specified property given the bounds on the input
+# This function performs Gradient Ascent on the specified property given the bounds on the input and, if it didn't lead
+# to positive loss, outputs the information about gradients
 
 def gradient_ascent(model, perturbed_image, lower_bound, upper_bound, true_label, test_label, pgd_learning_rate,
                     num_epochs):
-    # Initialise the relevant optimiser and the heuristics dictionary to be used if the PGD attack is unsuccessful
+    # Initialise the relevant optimiser and the list to store the gradients to be later used for heuristics generation
     optimizer = torch.optim.Adam([perturbed_image], lr=pgd_learning_rate)
-    heuristics_dict = {}  # TODO
+    gradients = []
 
     # Perform Gradient Ascent for a specified number of epochs
     for epoch in range(num_epochs):
@@ -87,6 +95,9 @@ def gradient_ascent(model, perturbed_image, lower_bound, upper_bound, true_label
         loss.backward()
         optimizer.step()
 
+        # Store the current gradient in the list
+        gradients.append(perturbed_image.grad.clone())
+
         # Clip the values of the perturbed image so that they are within the allowed perturbation magnitude
         # This operation isn't related to optimisation, hence it is wrapped with torch.no_grad()
         with torch.no_grad():
@@ -97,11 +108,14 @@ def gradient_ascent(model, perturbed_image, lower_bound, upper_bound, true_label
     if torch.max(model(perturbed_image)[0], 0)[1].item() == test_label:
         return True, None
 
-    # If the Gradient Ascent didn't lead to the changed prediction, then output the heuristics gathered during the
-    # Gradient Ascent and output them as well
+    # If the Gradient Ascent didn't lead to the changed prediction, then generate the dictionary containing information
+    # about gradients and output it as well
+    gradient_info_dict = generate_gradient_info_dict(gradients)
 
-    return False, heuristics_dict
+    return False, gradient_info_dict
 
+
+# This function calculates the loss based on the difference between the logits of the test and true classes
 
 def logit_difference_loss(logits, test_class, true_class):
     true_class_logit = logits[true_class]
@@ -109,4 +123,62 @@ def logit_difference_loss(logits, test_class, true_class):
     return test_class_logit - true_class_logit
 
 
-pgd_attack_properties('base_easy.pkl', 'cifar_base_kw', 1, 0.01, 100, 0)
+# This function generates the dictionary of containing information about gradients during the PGD attack
+
+def generate_gradient_info_dict(gradients):
+    # First take the absolute value since it is the magnitudes of gradients that matter since loss is always moving in
+    # the same direction (increasing) and the pixel values may decrease or increase
+    gradient_magnitudes = [torch.abs(gradient) for gradient in gradients]
+
+    # Initialise the output dictionary and add the last gradient magnitude to the dictionary straight away
+    gradient_info_dict = {'last gradient': gradient_magnitudes[-1]}
+
+    # Transform the list of consequent gradients into the list of pixel gradients
+    gradient_magnitudes_pixels = grouped_by_epoch_to_grouped_by_pixel(gradient_magnitudes)
+
+    # Compute pixel-wise mean, median, maximum and minimum gradients as well as the standard deviation and store them
+    mean_gradient_magnitudes_pixels = []
+    median_gradient_magnitudes_pixels = []
+    max_gradient_magnitudes_pixels = []
+    min_gradient_magnitudes_pixels = []
+    std_gradient_magnitudes_pixels = []
+
+    for i in range(len(gradient_magnitudes_pixels)):
+        mean_gradient_magnitudes_pixels.append(torch.mean(gradient_magnitudes_pixels[i]))
+        median_gradient_magnitudes_pixels.append(torch.median(gradient_magnitudes_pixels[i]))
+        max_gradient_magnitudes_pixels.append(torch.max(gradient_magnitudes_pixels[i]))
+        min_gradient_magnitudes_pixels.append(torch.min(gradient_magnitudes_pixels[i]))
+        std_gradient_magnitudes_pixels.append(torch.std(gradient_magnitudes_pixels[i]))
+
+    # Reshape the lists of pixel statistics so that image shape is preserved
+    mean_gradient_magnitude = torch.reshape(torch.tensor(mean_gradient_magnitudes_pixels), (1, 3, 32, 32))
+    median_gradient_magnitude = torch.reshape(torch.tensor(median_gradient_magnitudes_pixels), (1, 3, 32, 32))
+    max_gradient_magnitude = torch.reshape(torch.tensor(max_gradient_magnitudes_pixels), (1, 3, 32, 32))
+    min_gradient_magnitude = torch.reshape(torch.tensor(min_gradient_magnitudes_pixels), (1, 3, 32, 32))
+    std_gradient_magnitude = torch.reshape(torch.tensor(std_gradient_magnitudes_pixels), (1, 3, 32, 32))
+
+    # Add the above statistics to the heuristics dictionary
+    gradient_info_dict['mean gradient'] = mean_gradient_magnitude
+    gradient_info_dict['median gradient'] = median_gradient_magnitude
+    gradient_info_dict['maximum gradient'] = max_gradient_magnitude
+    gradient_info_dict['minimum gradient'] = min_gradient_magnitude
+    gradient_info_dict['gradient std'] = std_gradient_magnitude
+
+    return gradient_info_dict
+
+
+# This function transforms the list of gradient magnitudes where each list element is the tensor represents the gradient
+# at a particular epoch to the list where each element is the tensor representing the gradient of a particular pixel
+
+def grouped_by_epoch_to_grouped_by_pixel(gradients):
+    gradients_grouped_by_pixels = []
+    for matrix_idx in range(len(gradients[0][0])):
+        for row_idx in range(len(gradients[0][0][0])):
+            for value_idx in range(len(gradients[0][0][0][0])):
+                gradients_grouped_by_pixels.append(torch.cat([torch.tensor([gradients[i][0][matrix_idx][row_idx]
+                                                                            [value_idx].item()]) for i in
+                                                              range(len(gradients))]))
+    return gradients_grouped_by_pixels
+
+
+pgd_gnn_attack_properties('base_easy.pkl', 'cifar_base_kw', 1, 0.1, 10, 0, [0])
