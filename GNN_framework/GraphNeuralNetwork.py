"fixed errors due to initialization of unneeded gradient computation graphs"

GNN_framework/GraphNeuralNetwork.py
@@ -4,7 +4,8 @@ import copy
 from plnn.modules import Flatten
 from GNN_framework.helper_functions import transform_embedding_vectors, get_numbers_of_connecting_nodes
 from GNN_framework.auxiliary_neural_networks import ForwardInputUpdateNN, ForwardReluUpdateNN, ForwardOutputUpdateNN, \
-    BackwardReluUpdateNN, BackwardInputUpdateNN, ScoreComputationNN
+    BackwardReluUpdateNN, BackwardInputUpdateNN, BoundsUpdateNN
+from torch.nn import functional as f
 
 
 class GraphNeuralNetwork:
@@ -15,9 +16,9 @@ class GraphNeuralNetwork:
     # When the GNN is initialised, a graph is created which has the same structure as the input neural network but each
     # proper node (input, ReLU and output) now containing the corresponding embedding vector filled with zeros.
     # In addition, all the auxiliary neural networks are initialised including the ones which perform the forward and
-    # backward update operations and the one which computes the scores of update methods of all input nodes
+    # backward update operations and the one which computes the new bounds of all input nodes
     def __init__(self, neural_network, input_size, input_feature_size, relu_feature_size, output_feature_size,
-                 embedding_vector_size=64, auxiliary_hidden_size=64, num_update_methods=3):
+                 embedding_vector_size=64, auxiliary_hidden_size=64, training_mode=False):
         # Store the underlying neural network as a field of the GNN
         self.neural_network = neural_network
 
@@ -48,16 +49,16 @@ class GraphNeuralNetwork:
 
         # Now, initialise all the auxiliary neural networks
         self.forward_input_update_nn = ForwardInputUpdateNN(input_feature_size, auxiliary_hidden_size,
-                                                            embedding_vector_size)
+                                                            embedding_vector_size, training_mode)
         self.forward_relu_update_nn = ForwardReluUpdateNN(relu_feature_size, auxiliary_hidden_size,
-                                                          embedding_vector_size)
+                                                          embedding_vector_size, training_mode)
         self.forward_output_update_nn = ForwardOutputUpdateNN(output_feature_size, auxiliary_hidden_size,
-                                                              embedding_vector_size)
+                                                              embedding_vector_size, training_mode)
         self.backward_relu_update_nn = BackwardReluUpdateNN(relu_feature_size, auxiliary_hidden_size,
-                                                            embedding_vector_size)
+                                                            embedding_vector_size, training_mode)
         self.backward_input_update_nn = BackwardInputUpdateNN(input_feature_size, auxiliary_hidden_size,
-                                                              embedding_vector_size)
-        self.score_computation_nn = ScoreComputationNN(embedding_vector_size, auxiliary_hidden_size, num_update_methods)
+                                                              embedding_vector_size, training_mode)
+        self.bounds_update_nn = BoundsUpdateNN(embedding_vector_size, auxiliary_hidden_size, training_mode)
 
     def reset_input_embedding_vectors(self):
         """
@@ -97,8 +98,8 @@ class GraphNeuralNetwork:
         self.input_embeddings = self.input_embeddings.reshape(self.input_embeddings.size()[0], -1)
 
         # Perform the forward update on each input embedding vector in turn
-        for input_idx in range(self.input_embeddings.size()[1]):
-            self.input_embeddings[:, input_idx] = self.forward_input_update_nn(input_feature_vectors[:, input_idx])
+        for node_idx in range(self.input_embeddings.size()[-1]):
+            self.input_embeddings[:, node_idx] = self.forward_input_update_nn(input_feature_vectors[:, node_idx])
 
         # Reshape the input embedding vectors to have the same size as before the update, since it will be easier
         self.input_embeddings = self.input_embeddings.reshape(original_size)
@@ -111,7 +112,7 @@ class GraphNeuralNetwork:
         # Initialise the variable which will be counting the number of ReLUs, thus matching the appropriate features and
         # embedding vectors to corresponding ReLUs, and the variable which will keep track of the output of each layer
         relu_layer_idx = 0
-        embedding_vectors = self.input_embeddings
+        embedding_vectors = self.input_embeddings.clone()
 
         # Initialise an extra variable which will be needed during the backward update - the size before flattening
         size_before_flattening = torch.Size([0])
@@ -144,7 +145,7 @@ class GraphNeuralNetwork:
                 # Transform all the embedding vectors so that they can enter the appropriate neural network
                 transformed_embedding_vectors = transform_embedding_vectors(embedding_vectors, relu_feature_vectors)
 
-                # Update each embedding vector in turn
+                # Update the embedding vectors one at a time
                 for node_idx in range(embedding_vectors.size()[-1]):
                     embedding_vectors[:, node_idx] = self.forward_relu_update_nn(
                         relu_feature_vectors[:, node_idx], transformed_embedding_vectors[:, node_idx])
@@ -154,7 +155,7 @@ class GraphNeuralNetwork:
 
                 # Set the corresponding element of the storage to the updated embedding vectors tensor and increment the
                 # ReLU layer counter
-                self.relu_embeddings_list[relu_layer_idx] = embedding_vectors
+                self.relu_embeddings_list[relu_layer_idx] = embedding_vectors.clone()
                 relu_layer_idx += 1
 
             # Otherwise, the layer type hasn't been considered yet
@@ -169,7 +170,7 @@ class GraphNeuralNetwork:
             embedding_vectors[:, node_idx] = self.forward_output_update_nn(output_feature_vectors[:, node_idx],
                                                                            embedding_vectors[:, node_idx])
         embedding_vectors = embedding_vectors.reshape(original_size)
-        self.output_embeddings = embedding_vectors
+        self.output_embeddings = embedding_vectors.clone()
 
         return size_before_flattening
 
@@ -183,27 +184,32 @@ class GraphNeuralNetwork:
         # embedding vectors to corresponding ReLUs, and the variable which will keep track of the output of each layer
         # (now initialised at the output since this is a backward update)
         relu_layer_idx = len(self.relu_embeddings_list) - 1
-        embedding_vectors = self.output_embeddings
+        embedding_vectors = self.output_embeddings.clone()
 
         # Now go backward over each layer in turn, taking care to distinguish between the different types of layers
         for layer_idx, layer in enumerate(reversed(list(self.neural_network.children()))):
 
             # If the layer is linear, construct the layer with the same weights and zero biases, but passing in the
-            # opposite direction by swapping the inputs and outputs
+            # opposite direction by swapping the inputs and outputs. Also set all the required_grad parameters of this
+            # layer to False to avoid creating unnecessary gradient computation graphs
             if type(layer) == nn.Linear:
                 backwards_linear_layer = nn.Linear(layer.out_features, layer.in_features)
                 backwards_linear_layer.weight.data = torch.transpose(layer.weight.data, 1, 0)
                 backwards_linear_layer.bias.data = torch.zeros(backwards_linear_layer.bias.data.size())
+                for parameter in backwards_linear_layer.parameters():
+                    parameter.requires_grad = False
                 embedding_vectors = backwards_linear_layer(embedding_vectors)
 
             # If the layer is convolutional, construct the layer in the similar way to above (effectively performing
-            # deconvolution), but this time specifying the full set of parameters
+            # deconvolution), but this time specifying the full set of parameters. Also set requires_grad to False
             elif type(layer) == nn.Conv2d:
                 backwards_conv_layer = nn.ConvTranspose2d(layer.out_channels, layer.in_channels,
                                                           kernel_size=layer.kernel_size, stride=layer.stride,
                                                           padding=layer.padding, dilation=layer.dilation,
                                                           groups=layer.groups)
                 backwards_conv_layer.weight.data = layer.weight.data
+                for parameter in backwards_conv_layer.parameters():
+                    parameter.requires_grad = False
                 backwards_conv_layer.bias.data = torch.zeros(backwards_conv_layer.bias.data.size())
 
                 # If the next layer exists and is a ReLU layer or if this is the last layer (before the input one),
@@ -248,7 +254,7 @@ class GraphNeuralNetwork:
 
                 # Set the corresponding element of the storage to the updated embedding vectors tensor and decrement the
                 # ReLU layer counter
-                self.relu_embeddings_list[relu_layer_idx] = embedding_vectors
+                self.relu_embeddings_list[relu_layer_idx] = embedding_vectors.clone()
                 relu_layer_idx -= 1
 
             # Otherwise, the layer type hasn't been considered yet
@@ -263,34 +269,48 @@ class GraphNeuralNetwork:
             embedding_vectors[:, node_idx] = self.backward_input_update_nn(input_feature_vectors[:, node_idx],
                                                                            embedding_vectors[:, node_idx])
         embedding_vectors = embedding_vectors.reshape(original_size)
-        self.input_embeddings = embedding_vectors
+        self.input_embeddings = embedding_vectors.clone()
 
-    def compute_scores(self):
+    def compute_updated_bounds(self, old_lower_bound, old_upper_bound):
         """
-        This function computes the scores for all the input nodes
+        This function computes the updated lower and upper bounds of the input domain by passing the input embedding
+        vectors through the appropriate neural network and then constraining its output in a meaningful way.
         """
-        # Initialise the row tensor of tensors of scores where each tensor corresponds to the scores associated with a
-        # particular pixel (size hasn't been computed yet)
-        scores = torch.tensor([0])
-
         # Reshape the input embeddings to the size which is easy to iterate over, storing the original size
         original_size = self.input_embeddings.size()
         self.input_embeddings = self.input_embeddings.reshape(self.input_embeddings.size()[0], -1)
 
-        # For each input node, pass the corresponding embedding vector through the Score Computation NN
+        # Initialise the row tensor of tensors of new lower and upper bounds where each tensor corresponds to the new
+        # lower and upper bound associated with a particular pixel
+        new_lower_bound_and_offset = torch.zeros([2, self.input_embeddings.size()[-1]])
+
+        # For each input node, pass its embedding vector through the Bounds Update NN
         for input_idx in range(self.input_embeddings.size()[-1]):
-            pixel_scores = self.score_computation_nn(self.input_embeddings[:, input_idx])
-
-            # During the first loop, resize the tensor containing scores to the correct size
-            if input_idx == 0:
-                scores = torch.zeros(torch.Size([pixel_scores.size()[0], self.input_embeddings.size()[-1]]))
-
-            scores[:, input_idx] = self.score_computation_nn(self.input_embeddings[:, input_idx])
+            new_lower_bound_and_offset[:, input_idx] = self.bounds_update_nn(self.input_embeddings[:, input_idx])
 
         # Reshape the input embeddings to the original size
         self.input_embeddings = self.input_embeddings.reshape(original_size)
 
-        return scores
+        # Initialise the new lower and the offset tensors
+        new_lower_bound = new_lower_bound_and_offset[0, :].reshape(old_lower_bound.size())
+        offset = new_lower_bound_and_offset[1, :].reshape(old_lower_bound.size())
+
+        # Now the constraints should be applied on new lower bounds and offsets.
+        # 1. If the obtained lower bound is smaller than the old lower bound, set the new lower bound to the old one; if
+        # it is larger than the old upper bound, set it to the old upper bound
+        new_lower_bound = torch.max(new_lower_bound, old_lower_bound)
+        new_lower_bound = torch.min(new_lower_bound, old_upper_bound)
+
+        # 2. If some elements of the offset are negative, set them to zero
+        offset = f.relu(offset)
+
+        # Initialise the upper bound tensor
+        new_upper_bound = new_lower_bound + offset
+
+        # 3. Finally, if the new upper bound is bigger than the old upper bound, set it to the old upper bound
+        new_upper_bound = torch.min(new_upper_bound, new_lower_bound)
+
+        return new_lower_bound, new_upper_bound
 
     def parameters(self):
         """
@@ -302,9 +322,9 @@ class GraphNeuralNetwork:
                                self.forward_output_update_nn,
                                self.backward_relu_update_nn,
                                self.backward_input_update_nn,
-                               self.score_computation_nn]
+                               self.bounds_update_nn]
 
         # Now use the "yield" keyword to return the generator object on all the parameters of all the above networks
         for neural_network in gnn_neural_networks:
             for parameter in neural_network.parameters():
-                yield parameter
+                yield parameter.requires_grad_(True)

GNN_framework/attack_properties_with_pgd.py
@@ -1,8 +1,7 @@
 import torch
 from exp_utils.model_utils import load_verified_data, match_with_properties
 from GNN_framework.GraphNeuralNetwork import GraphNeuralNetwork
-from GNN_framework.helper_functions import match_with_subset, simplify_model, perturb_image, gradient_ascent, \
-    update_domain_bounds
+from GNN_framework.helper_functions import match_with_subset, simplify_model, perturb_image, gradient_ascent
 from GNN_framework.features_generation import generate_input_feature_vectors, generate_relu_output_feature_vectors
 
 
@@ -55,9 +54,10 @@ def pgd_gnn_attack_property(simplified_model, image, epsilon, epsilon_factor, pg
     # First, perturb the image randomly within the allowed bounds and perform a PGD attack
     lower_bound = torch.add(-epsilon * epsilon_factor, image)
     upper_bound = torch.add(epsilon * epsilon_factor, image)
-    perturbed_image = perturb_image(lower_bound, upper_bound).requires_grad_(True)
-    successful_attack_flag, gradient_info_dict = gradient_ascent(simplified_model, perturbed_image, lower_bound,
-                                                                 upper_bound, pgd_learning_rate, num_iterations)
+    perturbed_image = perturb_image(lower_bound, upper_bound)
+    successful_attack_flag, perturbed_image, gradient_info_dict = gradient_ascent(simplified_model, perturbed_image,
+                                                                                  lower_bound, upper_bound,
+                                                                                  pgd_learning_rate, num_iterations)
 
     # If the attack was successful, the procedure can be terminated and True can be returned
     if successful_attack_flag:
@@ -86,18 +86,16 @@ def pgd_gnn_attack_property(simplified_model, image, epsilon, epsilon_factor, pg
         # Perform a series of forward and backward updates of all the embedding vectors within the GNN
         gnn.update_embedding_vectors(input_feature_vectors, relu_feature_vectors_list, output_feature_vectors)
 
-        # Compute the scores for each image pixel
-        pixel_scores = gnn.compute_scores()
-
         # Update the domain bounds for each pixel based on the pixel scores above
-        lower_bound, upper_bound = update_domain_bounds(lower_bound, upper_bound, pixel_scores)
+        lower_bound, upper_bound = gnn.compute_updated_bounds(lower_bound, upper_bound)
 
         # Perturb each pixel within the updated domain bounds
-        perturbed_image = perturb_image(lower_bound, upper_bound).requires_grad_(True)
+        perturbed_image = perturb_image(lower_bound, upper_bound)
 
         # Perform a PGD attack given the new bounds and perturbation
-        successful_attack_flag, gradient_info_dict = gradient_ascent(simplified_model, perturbed_image, lower_bound,
-                                                                     upper_bound, pgd_learning_rate, num_iterations)
+        successful_attack_flag, perturbed_image, gradient_info_dict = gradient_ascent(simplified_model, perturbed_image,
+                                                                                      lower_bound, upper_bound,
+                                                                                      pgd_learning_rate, num_iterations)
 
         # If the attack was successful, the procedure can be terminated and True can be returned, otherwise continue
         if successful_attack_flag:
@@ -118,7 +116,7 @@ def pgd_gnn_attack_property(simplified_model, image, epsilon, epsilon_factor, pg
 
 
 def main():
-    print(pgd_gnn_attack_properties('base_easy.pkl', 'cifar_base_kw', 1, 0.1, 10, 10, subset=[0]))
+    print(pgd_gnn_attack_properties('base_easy.pkl', 'cifar_base_kw', 1, 0.1, 10, 2, subset=[0]))
 
 
 if __name__ == '__main__':

GNN_framework/auxiliary_neural_networks.py
@@ -7,10 +7,17 @@ class ForwardInputUpdateNN(nn.Module):
     """
     This class represents the neural network which performs the forward update on the input nodes
     """
-    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size):
+    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size, training_mode):
         super(ForwardInputUpdateNN, self).__init__()
         self.linear_1 = nn.Linear(feature_vector_size, hidden_layer_size)
         self.linear_2 = nn.Linear(hidden_layer_size, embedding_vector_size)
+
+        # If the training mode is off, then set all the required_grad parameters of all the layer parameters to False
+        if not training_mode:
+            for parameter in self.linear_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_2.parameters():
+                parameter.requires_grad = False
 
     def forward(self, input_feature_vector):
         return self.linear_2(f.relu(self.linear_1(input_feature_vector)))
@@ -20,7 +27,7 @@ class ForwardReluUpdateNN(nn.Module):
     """
     This class represents the neural network which performs the forward update on the ReLU hidden layer nodes
     """
-    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size):
+    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size, training_mode):
         super(ForwardReluUpdateNN, self).__init__()
 
         # Initialise the layers for obtaining information from the local features
@@ -35,6 +42,20 @@ class ForwardReluUpdateNN(nn.Module):
         # embedding vectors
         self.linear_combine_1 = nn.Linear(2 * hidden_layer_size, hidden_layer_size)
         self.linear_combine_2 = nn.Linear(hidden_layer_size, embedding_vector_size)
+
+        if not training_mode:
+            for parameter in self.linear_local_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_local_2.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_neighbour_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_neighbour_2.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_2.parameters():
+                parameter.requires_grad = False
 
     def forward(self, local_feature_vector, transformed_embedding_vector):
         # First, get information from the local feature vector. If the hidden layer node currently considered is
@@ -60,7 +81,7 @@ class ForwardOutputUpdateNN(nn.Module):
     """
     This class represents the neural network which performs the forward update on the output node
     """
-    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size):
+    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size, training_mode):
         super(ForwardOutputUpdateNN, self).__init__()
 
         # Initialise the layer for obtaining information from the local features
@@ -69,6 +90,14 @@ class ForwardOutputUpdateNN(nn.Module):
         # Initialise the layers for combining information from the local features and the last hidden layer embeddings
         self.linear_combine_1 = nn.Linear(hidden_layer_size + embedding_vector_size, hidden_layer_size)
         self.linear_combine_2 = nn.Linear(hidden_layer_size, embedding_vector_size)
+
+        if not training_mode:
+            for parameter in self.linear_local.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_2.parameters():
+                parameter.requires_grad = False
 
     def forward(self, local_feature_vector, propagated_last_hidden_layer_embeddings):
         # First, get information from the local feature vector
@@ -83,13 +112,13 @@ class BackwardReluUpdateNN(nn.Module):
     """
     This class represents the neural network which performs the backward update on the hidden layer nodes
     """
-    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size):
+    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size, training_mode):
         super(BackwardReluUpdateNN, self).__init__()
 
         # Initialise the layers for obtaining information from the local features
-        self.linear_local_1_1 = nn.Linear(feature_vector_size, hidden_layer_size)
-        self.linear_local_1_2 = nn.Linear(hidden_layer_size, hidden_layer_size)
-        self.linear_local_1_3 = nn.Linear(hidden_layer_size, hidden_layer_size)
+        self.linear_local_1 = nn.Linear(feature_vector_size, hidden_layer_size)
+        self.linear_local_2 = nn.Linear(hidden_layer_size, hidden_layer_size)
+        self.linear_local_3 = nn.Linear(hidden_layer_size, hidden_layer_size)
 
         # Initialise the layers for obtaining information from the next neighbour embedding vectors
         self.linear_neighbour_1 = nn.Linear(2 * embedding_vector_size, hidden_layer_size)
@@ -100,16 +129,32 @@ class BackwardReluUpdateNN(nn.Module):
         self.linear_combine_1 = nn.Linear(2 * hidden_layer_size, hidden_layer_size)
         self.linear_combine_2 = nn.Linear(hidden_layer_size, embedding_vector_size)
 
+        if not training_mode:
+            for parameter in self.linear_local_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_local_2.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_local_3.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_neighbour_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_neighbour_2.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_2.parameters():
+                parameter.requires_grad = False
+
     def forward(self, local_feature_vector, transformed_next_layer_embeddings):
         # First, implement the 1st stage of getting information about the local feature vector. If the hidden layer node
         # currently considered is unambiguous (its relaxation triangle intercept which is the last feature is zero), set
         # the output from the first stage to zero the vector
         if local_feature_vector[-1].item() == 0.0:
-            local_features_info = torch.zeros(self.linear_local_1_3.out_features)
+            local_features_info = torch.zeros(self.linear_local_3.out_features)
         # Otherwise, pass it through the layers of the 1st stage network
         else:
-            local_features_info = self.linear_local_1_3(f.relu(self.linear_local_1_2(f.relu(
-                self.linear_local_1_1(local_feature_vector)))))
+            local_features_info = self.linear_local_3(f.relu(self.linear_local_2(f.relu(
+                self.linear_local_1(local_feature_vector)))))
 
         # Second, get information from the transformed next neighbour embedding vectors
         next_neighbour_embeddings_info = self.linear_neighbour_2(f.relu(
@@ -125,7 +170,7 @@ class BackwardInputUpdateNN(nn.Module):
     """
     This class represents the neural network which performs the backward update on the input nodes
     """
-    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size):
+    def __init__(self, feature_vector_size, hidden_layer_size, embedding_vector_size, training_mode):
         super(BackwardInputUpdateNN, self).__init__()
 
         # Initialise the layers for obtaining information from the local features
@@ -136,6 +181,16 @@ class BackwardInputUpdateNN(nn.Module):
         self.linear_combine_1 = nn.Linear(hidden_layer_size + embedding_vector_size, hidden_layer_size)
         self.linear_combine_2 = nn.Linear(hidden_layer_size, embedding_vector_size)
 
+        if not training_mode:
+            for parameter in self.linear_local_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_local_2.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_combine_2.parameters():
+                parameter.requires_grad = False
+
     def forward(self, local_feature_vector, propagated_first_hidden_layer_embeddings):
         # First, get information from the local feature vector
         local_features_info = self.linear_local_2(f.relu(self.linear_local_1(local_feature_vector)))
@@ -145,16 +200,23 @@ class BackwardInputUpdateNN(nn.Module):
         return self.linear_combine_2(f.relu(self.linear_combine_1(combined_info)))
 
 
-class ScoreComputationNN(nn.Module):
+class BoundsUpdateNN(nn.Module):
     """
-    This class represents the neural network which computes the scores for all possible input domain update methods
+    This class represents the neural network which takes the input embedding vector and outputs a 2-dimensional tensor.
+    Its first elements is the new updated lower bound and its second element is the offset from the new lower bound.
     """
-    def __init__(self, embedding_vector_size, hidden_layer_size, number_of_update_methods):
-        super(ScoreComputationNN, self).__init__()
+    def __init__(self, embedding_vector_size, hidden_layer_size, training_mode):
+        super(BoundsUpdateNN, self).__init__()
 
         # Assuming this network is a 2-layer fully-connected network, initialise the two required layers
         self.linear_1 = nn.Linear(embedding_vector_size, hidden_layer_size)
-        self.linear_2 = nn.Linear(hidden_layer_size, number_of_update_methods)
+        self.linear_2 = nn.Linear(hidden_layer_size, 2)
+
+        if not training_mode:
+            for parameter in self.linear_1.parameters():
+                parameter.requires_grad = False
+            for parameter in self.linear_2.parameters():
+                parameter.requires_grad = False
 
     def forward(self, input_embedding_vector):
         return self.linear_2(f.relu(self.linear_1(input_embedding_vector)))

GNN_framework/helper_functions.py
@@ -41,11 +41,14 @@ def perturb_image(lower_bound, upper_bound):
 
 
 def gradient_ascent(simplified_model, perturbed_image, lower_bound, upper_bound, pgd_learning_rate, num_iterations,
-                    return_loss=False, retain_graph=False):
+                    return_loss=False):
     """
     This function performs Gradient Ascent on the specified property given the bounds on the input and, if it didn't
-    lead to positive loss, outputs the information about gradients
+    lead to positive loss, outputs the information about gradients and the last version of the optimized variable.
     """
+    # First of all, set the requires_grad parameter of the perturbed image to True to make it suitable for optimization
+    perturbed_image.requires_grad = True
+
     # Initialise the relevant optimiser and the list to store the gradients to be later used for gradient information
     optimizer = torch.optim.Adam([perturbed_image], lr=pgd_learning_rate)
     gradients = []
@@ -53,27 +56,27 @@ def gradient_ascent(simplified_model, perturbed_image, lower_bound, upper_bound,
 
     # Perform Gradient Ascent for a specified number of epochs
     for iteration in range(num_iterations):
-        # The output of the network is the difference between the logits of the correct and the test class which is
-        # the same as -loss, but since Gradient Ascent is performed, it is this difference that must be minimised
+        # The output of the network is the difference between the logits of the correct and the test class which is the
+        # same as -loss, but since Gradient Ascent is performed, it is this difference that must be minimised
         loss = simplified_model(perturbed_image)
 
-        # If the difference between the logit of the test class and the logit of the true class is positive,
-        # then the PGD attack was successful and gradient ascent can be stopped
+        # If the difference between the logit of the test class and the logit of the true class is positive, then the
+        # PGD attack was successful and gradient ascent can be stopped
         if loss < 0:
             if return_loss:
                 return loss
             else:
-                return True, None
+                return True, None, None
 
         optimizer.zero_grad()
-        loss.backward(retain_graph=retain_graph)
+        loss.backward()
         optimizer.step()
 
         # Store the current gradient in the list
         gradients.append(perturbed_image.grad.clone())
 
-        # Clip the values of the perturbed image so that they are within the allowed perturbation magnitude
-        # This operation isn't related to optimisation, hence it is wrapped with torch.no_grad()
+        # Clip the values of the perturbed image so that they are within the allowed perturbation magnitude. This
+        # operation isn't related to optimisation, hence it is wrapped with torch.no_grad()
         with torch.no_grad():
             perturbed_image[:] = torch.max(torch.min(perturbed_image, upper_bound), lower_bound)
 
@@ -83,16 +86,20 @@ def gradient_ascent(simplified_model, perturbed_image, lower_bound, upper_bound,
         if return_loss:
             return loss
         else:
-            return True, None
+            return True, None, None
 
     # If the Gradient Ascent didn't lead to the changed prediction, then generate the dictionary containing information
     # about gradients and output it as well
     gradient_info_dict = generate_gradient_info_dict(gradients)
 
+    # Set the requires_grad parameter of the perturbed image to False so that gradients with respect to it aren't
+    # computed outside of this function
+    perturbed_image.requires_grad = False
+
     if return_loss:
         return loss
     else:
-        return False, gradient_info_dict
+        return False, perturbed_image, gradient_info_dict
 
 
 def generate_gradient_info_dict(gradients):
@@ -110,18 +117,18 @@ def generate_gradient_info_dict(gradients):
     gradient_magnitudes_pixels = grouped_by_iteration_to_grouped_by_pixel(gradient_magnitudes)
 
     # Compute pixel-wise mean, median, maximum and minimum gradients as well as the standard deviation and store them
-    mean_gradient_magnitudes_pixels = []
-    median_gradient_magnitudes_pixels = []
-    max_gradient_magnitudes_pixels = []
-    min_gradient_magnitudes_pixels = []
-    std_gradient_magnitudes_pixels = []
+    mean_gradient_magnitudes_pixels = torch.zeros(gradient_info_dict['last gradient'].size())
+    median_gradient_magnitudes_pixels = torch.zeros(gradient_info_dict['last gradient'].size())
+    max_gradient_magnitudes_pixels = torch.zeros(gradient_info_dict['last gradient'].size())
+    min_gradient_magnitudes_pixels = torch.zeros(gradient_info_dict['last gradient'].size())
+    std_gradient_magnitudes_pixels = torch.zeros(gradient_info_dict['last gradient'].size())
 
     for pixel_idx in range(len(gradient_magnitudes_pixels)):
-        mean_gradient_magnitudes_pixels.append(torch.mean(gradient_magnitudes_pixels[pixel_idx]))
-        median_gradient_magnitudes_pixels.append(torch.median(gradient_magnitudes_pixels[pixel_idx]))
-        max_gradient_magnitudes_pixels.append(torch.max(gradient_magnitudes_pixels[pixel_idx]))
-        min_gradient_magnitudes_pixels.append(torch.min(gradient_magnitudes_pixels[pixel_idx]))
-        std_gradient_magnitudes_pixels.append(torch.std(gradient_magnitudes_pixels[pixel_idx]))
+        mean_gradient_magnitudes_pixels[pixel_idx] = torch.mean(gradient_magnitudes_pixels[pixel_idx])
+        median_gradient_magnitudes_pixels[pixel_idx] = torch.median(gradient_magnitudes_pixels[pixel_idx])
+        max_gradient_magnitudes_pixels[pixel_idx] = torch.max(gradient_magnitudes_pixels[pixel_idx])
+        min_gradient_magnitudes_pixels[pixel_idx] = torch.min(gradient_magnitudes_pixels[pixel_idx])
+        std_gradient_magnitudes_pixels[pixel_idx] = torch.std(gradient_magnitudes_pixels[pixel_idx])
 
     # Add the above statistics to the gradient information dictionary
     gradient_info_dict['mean gradient'] = mean_gradient_magnitudes_pixels
@@ -151,46 +158,6 @@ def grouped_by_iteration_to_grouped_by_pixel(gradients):
         row_gradients_grouped_by_pixel.append(torch.tensor(pixel_gradients_list))
 
     return row_gradients_grouped_by_pixel
-
-
-def update_domain_bounds(old_lower_bound, old_upper_bound, scores):
-    """
-    This function updates the domain bounds based on the scores. For each input node, it chooses the update method
-    corresponding to the maximum score among all scores for that node
-    """
-    # First, reshape the old lower and upper bounds to be row tensors
-    old_lower_bound_row = old_lower_bound.view(-1)
-    old_upper_bound_row = old_upper_bound.view(-1)
-
-    # Initialise the new lower and upper bound tensors
-    new_lower_bound_row = torch.zeros(old_lower_bound_row.size())
-    new_upper_bound_row = torch.zeros(old_upper_bound_row.size())
-
-    # The size of each score tensor represents the number of update methods chosen before. It is an odd number since one
-    # update method is always for a node to retain the old bounds and all the other update methods (of which there is an
-    # even number) symmetrically divide the original domain bounds with one division boundary being at the centre of the
-    # old domain. Each score tensor  Make the decision for each input node
-    for i in range(old_lower_bound_row.size()[0]):
-        # Find the maximum score index in the corresponding score tensor
-        max_score_index = torch.argmax(scores[:, i]).item()
-
-        # If the maximum score index is the first index, then retain the bounds as they are
-        if max_score_index == 0:
-            new_lower_bound_row[i] = old_lower_bound_row[i]
-            new_upper_bound_row[i] = old_upper_bound_row[i]
-
-        # Otherwise update the bounds according to the approach outlined above
-        else:
-            new_lower_bound_row[i] = old_lower_bound_row[i] + (max_score_index - 1) \
-                                     * (old_upper_bound_row[i] - old_lower_bound_row[i]) / (scores[0].size()[0] - 1)
-            new_upper_bound_row[i] = old_lower_bound_row[i] + max_score_index * (
-                    old_upper_bound_row[i] - old_lower_bound_row[i]) / (scores[0].size()[0] - 1)
-
-    # Finally, reshape the new lower and upper bounds so that they have the image shape
-    new_lower_bound = new_lower_bound_row.reshape(old_lower_bound.size())
-    new_upper_bound = new_upper_bound_row.reshape(old_upper_bound.size())
-
-    return new_lower_bound, new_upper_bound
 
 
 def transform_embedding_vectors(embedding_vectors, local_feature_vectors):
@@ -244,13 +211,15 @@ def get_numbers_of_connecting_nodes(backwards_conv_layer, input_size):
 
     # Construct a copy of the passed convolutional layer but with 1 input and 1 output channel. Also set all weights of
     # the layer to ones and biases to zeros so that the numbers of connecting nodes appear in the output for each output
-    # node
+    # node. Finally, set all the required_grad parameters of its parameters to False
     modified_layer = torch.nn.ConvTranspose2d(1, 1, kernel_size=backwards_conv_layer.kernel_size,
                                               stride=backwards_conv_layer.stride, padding=backwards_conv_layer.padding,
                                               dilation=backwards_conv_layer.dilation,
                                               groups=backwards_conv_layer.groups)
     modified_layer.weight.data = torch.ones(modified_layer.weight.data.size())
     modified_layer.bias.data = torch.zeros(modified_layer.bias.data.size())
+    for parameter in modified_layer.parameters():
+        parameter.requires_grad = False
 
     # Now pass the test input through the modified layer to obtain, for example, a tensor of size [1, 1, _, _]. It was
     # checked that when a tensor of larger size, e.g. [4, 16, _, _] is divided by it, each of the [_, _] matrices

GNN_training/train_GNN.py
@@ -39,8 +39,8 @@ def generate_gnn_training_parameters(training_dataset_filename, model_name, gnn_
             # Simplify the model according to the current true and test class labels
             simplified_model = simplify_model(model, feature_dict['true label'], feature_dict['test label'])
 
-            # When the epoch is not the first one, reset the input embedding vectors since the forward input update
-            # function only activates when the input embedding vectors are zero
+            # When the epoch and the subdomain index are not the first one, reset the input embedding vectors since the
+            # forward input update function only activates when the input embedding vectors are zero
             if epoch != 0 and subdomain_index != 0:
                 gnn.reset_input_embedding_vectors()
 
@@ -48,19 +48,20 @@ def generate_gnn_training_parameters(training_dataset_filename, model_name, gnn_
             gnn.update_embedding_vectors(feature_dict['input'], feature_dict['hidden'], feature_dict['output'])
 
             # Compute the scores for each image pixel
-            pixel_scores = gnn.compute_scores()
+            pixel_scores = gnn.compute_updated_bounds()
 
             # Update the domain bounds for each pixel based on the pixel scores above
             old_lower_bound = feature_dict['input'][0, :].reshape(temp_input_size)
             old_upper_bound = feature_dict['input'][1, :].reshape(temp_input_size)
             new_lower_bound, new_upper_bound = update_domain_bounds(old_lower_bound, old_upper_bound, pixel_scores)
 
-            # Perturb each pixel within the updated domain bounds
-            perturbed_image = perturb_image(new_lower_bound, new_upper_bound).requires_grad_(True)
-
-            # Perform a PGD attack given the new bounds and perturbation
-            loss = gradient_ascent(simplified_model, perturbed_image, new_lower_bound, new_upper_bound,
-                                   pgd_learning_rate, num_iterations, return_loss=True, retain_graph=True)
+            # # Perturb each pixel within the updated domain bounds
+            # perturbed_image = perturb_image(new_lower_bound, new_upper_bound)
+            #
+            # # Perform a PGD attack given the new bounds and perturbation
+            # loss = gradient_ascent(simplified_model, perturbed_image, new_lower_bound, new_upper_bound,
+            #                        pgd_learning_rate, num_iterations, return_loss=True)
+            loss = simplified_model(torch.add(new_upper_bound, new_lower_bound) / 2)
 
             # Make the optimizer step in a usual manner
             optimizer.zero_grad()
@@ -69,7 +70,7 @@ def generate_gnn_training_parameters(training_dataset_filename, model_name, gnn_
 
 
 def main():
-    generate_gnn_training_parameters('training_dataset.pkl', 'cifar_base_kw', 0.1, 0.1, 10, 10, '')
+    generate_gnn_training_parameters('training_dataset.pkl', 'cifar_base_kw', 0.1, 0.1, 10, 2, '')
 
 
 if __name__ == '__main__':