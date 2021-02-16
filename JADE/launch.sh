7ebbad58e5dbf488a8e5510b41473717fb19d508
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2021-02-16 14:38:26 +0000

    "changed the requested time in launch script"

--- JADE/launch.sh
+++ JADE/launch.sh
@@ -4,7 +4,7 @@ exp_num=$SLURM_ARRAY_TASK_ID
 module purge
 # module load python3/anaconda
 module load cuda/10.1
-export PATH=$PATH:/jmain01/home/JAD035/pkm01/axa50-pkm01
+export PATH=$PATH:/jmain01/home/JAD035/pkm01/axa50-pkm01/miniconda3/bin
 source activate /jmain01/home/JAD035/pkm01/axa50-pkm01/miniconda3/venv/Scripts
 # source activate /jmain01/home/JAD035/pkm01/axa50-pkm01/anaconda3/envs/python_38
 # export VISION_DATA="/jmain01/home/JAD035/pkm01/fxj15-pkm01/data/"
