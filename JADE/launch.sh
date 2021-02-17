8894deb7ff8dbfb3c0f7098c245c6f463c9bd8b9
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2021-02-17 20:55:29 +0000

    "commit"

--- JADE/launch.sh
+++ JADE/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo hostid
+hostid
 exp_num=$SLURM_ARRAY_TASK_ID
 
 module purge
@@ -8,7 +8,6 @@ module load cuda/10.1
 
 export PATH=$PATH:/jmain01/home/JAD035/pkm01/axa50-pkm01/miniconda3/bin
 
-module load Gurobi/9.1.1
 export GRB_LICENSE_FILE=/jmain01/home/JAD035/pkm01/axa50-pkm01/gurobi911/gurobi.lic
 
 source activate /jmain01/home/JAD035/pkm01/axa50-pkm01/miniconda3/envs/GNN_Attention_Mechanism
