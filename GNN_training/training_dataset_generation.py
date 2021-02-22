e173e9af148a7ae696ba15537ecb19f359c581bf
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2021-02-22 15:40:05 +0000

    "setup for training dataset generation"

--- GNN_training/training_dataset_generation.py
+++ GNN_training/training_dataset_generation.py
@@ -96,7 +96,7 @@ def main():
     log_filename = 'training_dataset_generation_log_' + str(args.start_index) + '_' + str(args.end_index) + '.txt'
 
     generate_training_dataset('train_SAT_jade.pkl', 'cifar_base_kw', 0.01, 2000, output_filename, log_filename,
-                              device='cuda', timeout=60, subset=subset)
+                              device='cuda', subset=subset)
 
 
 if __name__ == '__main__':
