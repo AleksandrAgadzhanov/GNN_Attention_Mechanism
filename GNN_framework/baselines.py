20abda9bfda02173361786f81011b7f6c27f68f5
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2021-04-01 13:53:02 +0100

    "setup for baseline vs heuristics experiment"

--- GNN_framework/baselines.py
+++ GNN_framework/baselines.py
@@ -78,11 +78,11 @@ def main():
     parser.add_argument('--filename', type=str)
     args = parser.parse_args()
     properties_filename = args.filename + '.pkl'
-    log_filepath = 'experiment_results/' + args.filename + '_log.txt'
+    log_filepath = 'GNN_framework/' + args.filename + '_log.txt'
     output_filename = args.filename + '_dict.pkl'
 
-    pgd_attack_properties(properties_filename, 'cifar_base_kw', 1.0, 0.1, 100, 51, output_filename,
-                          log_filepath=log_filepath, subset=list(range(100)))
+    pgd_attack_properties(properties_filename, 'cifar_base_kw', 1.5, 0.1, 100, 101, output_filename,
+                          log_filepath=log_filepath, subset=list(range(50)))
 
 
 if __name__ == '__main__':
