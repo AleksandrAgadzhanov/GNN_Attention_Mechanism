8f862380a3f5473a3b6b9b9e4e0cc256d958ccdf
Aleksandr Agadzhanov <alexagadzhanov97@gmail.com>
2021-04-16 18:59:44 +0100

    "setup for GNN testing (5 GNN zooms) and 2nd iteration of cross-validation (6 GNN zooms)"

--- experiment_results/plotter.py
+++ experiment_results/plotter.py
@@ -126,12 +126,12 @@ def plot_attack_success_rates(filepath_gnn_dict, filepath_baseline_dict, title):
 
 
 def main():
-    plot_training_loss('GNN_5_zooms/training_dict_5_zooms.pkl')
+    # plot_training_loss('GNN_5_zooms/training_dict_5_zooms.pkl')
     # plot_cross_validation_results(directory='../experiment_results/GNN_6_zooms/cross_validation_1st_iteration/',
     #                               space='log')
-    # plot_attack_success_rates('GNN_4_zooms/gnn_4_zooms_dict_easy_test_dataset.pkl',
-    #                           '../experiment_results/baseline_dict_easy_test_dataset.pkl',
-    #                           '')
+    plot_attack_success_rates('GNN_4_zooms/gnn_4_zooms_dict_easy_test_dataset.pkl',
+                              '../experiment_results/baseline_dict_easy_test_dataset.pkl',
+                              '')
 
 
 if __name__ == '__main__':
